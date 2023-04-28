332f794072d11c3c435ce17cb8f488c37ef5f8a2
PatStiles <pdstiles78@gmail.com>
2023-04-28 11:45:31 -0500

    feat: add pos sub() test

--- halo2-base/src/gates/tests/flex_gate_test.rs
+++ halo2-base/src/gates/tests/flex_gate_test.rs
@@ -16,3 +16,12 @@ fn test_add<F: ScalarField>(inputs: [QuantumCell<F>; 2]) -> F {
     let a = chip.add(ctx, inputs[0], inputs[1]);
     *a.value()
 }
+
+#[test_case([1, 1].map(Fr::from).map(Witness) => Fr::from(0) ; "sub(): 1 - 1 = 0")]
+fn test_sub<F: ScalarField>(inputs: [QuantumCell<F>; 2]) -> F {
+    let mut builder = GateThreadBuilder::mock();
+    let ctx = builder.main(0);
+    let chip = GateChip::default();
+    let a = chip.sub(ctx, inputs[0], inputs[1]);
+    *a.value()
+}
