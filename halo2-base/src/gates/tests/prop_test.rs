950f2f0dbf3b2ff7185bc5e75a15a80257bbe232
PatStiles <pdstiles78@gmail.com>
2023-05-09 18:00:25 -0500

    feat: add neg pranking test for idx_to_indicator

--- halo2-base/src/gates/tests/prop_test.rs
+++ halo2-base/src/gates/tests/prop_test.rs
@@ -1,6 +1,11 @@
 use proptest::{prelude::*, collection::vec};
-use crate::{gates::tests::{Fr, flex_gate_test, range_gate_test}, utils::ScalarField};
+use crate::{gates::{GateChip, GateInstructions, builder::{GateCircuitBuilder, GateThreadBuilder}, tests::{Fr, flex_gate_test, range_gate_test}}, utils::ScalarField};
+use crate::halo2_proofs::plonk::Assigned;
 use crate::{QuantumCell, QuantumCell::Witness};
+use itertools::Itertools;
+use halo2_proofs_axiom::dev::MockProver;
+use rand::rngs::OsRng;
+use ff::Field;
 
 prop_compose! {
     fn rand_fr()(val in any::<u64>()) -> Fr {
@@ -16,6 +21,76 @@ prop_compose! {
     fn sum_products_with_coeff_and_var_strat(max_length: usize)(val in vec((rand_fr(), rand_witness(), rand_witness()), 1..=max_length), witness in rand_witness()) -> (Vec<(Fr, QuantumCell<Fr>, QuantumCell<Fr>)>, QuantumCell<Fr>) {
         (val, witness)
     }
+}
+prop_compose! {
+    // length == 1 is just selecting [0] which should be covered in unit test
+    fn idx_to_indicator_strat(max_size: usize)
+        (k in 8u32..=20u32, idx_val in prop::sample::select(vec![Fr::zero(), Fr::one(), Fr::random(OsRng)]), len in 2usize..=max_size)
+        (k in Just(k), idx in 0..len, idx_val in Just(idx_val), len in Just(len), mut witness_vals in arb_indicator::<Fr>(len)) -> (u32, usize, usize, Vec<Fr>) {
+        witness_vals[idx] = idx_val;
+        (k, len, idx, witness_vals)
+    }
+}
+
+fn arb_indicator<F: ScalarField>(max_size: usize) -> impl Strategy<Value = Vec<F>> {
+    vec(Just(0), max_size).prop_map(|val| {
+        val.iter().map(|&x| F::from(x)).collect::<Vec<_>>()
+    })
+}
+
+fn check_instance(idx: usize, len: usize, ind_witnesses: &[Fr]) -> bool {   
+    // check that:
+    // the length of the witnes array is correct
+    // the sum of the witnesses is 1, indicting that there is only one index that is 1
+    if ind_witnesses.len() != len || 
+    ind_witnesses.iter().fold(Fr::zero(), |acc, val| { acc + *val }) != Fr::one() {
+        return false;
+    }
+    // Check that all indexes are zero except for the one at idx
+   for (i, v) in ind_witnesses.iter().enumerate() {
+        if i != idx && *v != Fr::zero() {
+            return false;
+        }
+   }
+   true 
+}
+
+// add filter to distinguish between valid and invalid output at end
+fn prop_test_idx_to_indicator_gen(k: u32, len: usize, idx: usize, ind_witnesses: &[Fr]) -> bool {
+    println!("ind_witnesses: {:?}", ind_witnesses);
+    // first create proving and verifying key
+    let mut builder = GateThreadBuilder::mock();
+    let gate = GateChip::default();
+    // assign value to advice column before by assigning `idx` via ctx.load() -> use same method as ind_offsets to get offset
+    let dummy_idx = Witness(Fr::from(idx as u64));
+    let indicator = gate.idx_to_indicator(builder.main(0), dummy_idx, len);
+    // get the offsets of the indicator cells for later 'pranking'
+    builder.config(k as usize, Some(9));
+    let ind_offsets = indicator.iter().map(|ind| ind.cell.unwrap().offset).collect::<Vec<_>>();
+    // prank the indicator cells
+    // TODO: prank the entire advice column with random values
+    for (offset, witness) in ind_offsets.iter().zip_eq(ind_witnesses) {
+        builder.main(0).advice[*offset] = Assigned::Trivial(*witness);
+    }
+    // Get idx and indicator from advice column
+    // Apply check instance function to `idx` and `ind_witnesses`
+    let circuit = GateCircuitBuilder::mock(builder); // no break points
+    // Check soundness of witness values
+    let is_valid_witness = check_instance(idx, len, ind_witnesses);
+    match MockProver::run(k as u32, &circuit, vec![]).unwrap().verify() {
+            // if the proof is valid, then the instance should be valid -> return true
+            Ok(_) => {
+                println!("Ok: is_valid_witness {:?}", is_valid_witness);
+                is_valid_witness == true
+            },
+            // if the proof is invalid, ignore
+            Err(e) =>  {
+                println!("Err: is_valid_witness {:?}", is_valid_witness);
+                println!("Error: {:?}", e);
+                is_valid_witness == false
+            },
+    }
+
 }
 
 // Ground truth functions
@@ -364,7 +439,6 @@ proptest! {
         prop_assert_eq!(result, ground_truth);
     }
 
-    // Range Check Property Tests
     #[test]
     fn prop_test_is_less_than(inputs in (rand_witness(), rand_witness(), 1..=16_usize)) {
         let ground_truth = is_less_than_ground_truth((*inputs.0.value(), *inputs.1.value()));
@@ -401,22 +475,28 @@ proptest! {
     }
 
     #[test]
-    fn prop_test_range_check(inputs in (rand_fr(), 1..=16_usize)) {
-        range_gate_test::test_range_check((inputs.0, inputs.1));
+    fn prop_test_range_check(inputs in (rand_fr(), any::<usize>().prop_filter("Non-zero upper bound", |x| *x != 0usize))) {
+        prop_assert_eq!(range_gate_test::test_range_check((inputs.0, inputs.1)), ());
     }
 
     #[test]
-    fn prop_test_check_less_than(inputs in (rand_witness(), rand_witness(), 1..=16_usize)) {
-        range_gate_test::test_check_less_than(([inputs.0, inputs.1], inputs.2));
+    fn prop_test_check_less_than(inputs in (rand_witness(), rand_witness(), any::<usize>().prop_filter("Non-zero upper bound", |x| *x != 0usize))) {
+        range_gate_test::test_check_less_than(([inputs.0, inputs.1], inputs.2))
     }
 
     #[test]
     fn prop_test_check_less_than_safe(inputs in (rand_fr(), any::<u64>().prop_filter("Non-zero upper bound", |x| *x != 0u64))) {
-        range_gate_test::test_check_less_than_safe((inputs.0, inputs.1));
+        range_gate_test::test_check_less_than_safe((inputs.0, inputs.1))
     }
 
     #[test]
     fn prop_test_check_big_less_than_safe(inputs in (rand_fr(), any::<u64>().prop_filter("Non-zero upper bound", |x| *x != 0u64))) {
-        range_gate_test::test_check_big_less_than_safe((inputs.0, inputs.1));
+        range_gate_test::test_check_big_less_than_safe((inputs.0, inputs.1))
+    }
+
+    // Negative tests
+    #[test]
+    fn test_neg_idx_to_indicator_gen((k, len, idx, witness_vals) in idx_to_indicator_strat(100)) {
+        prop_assert!(prop_test_idx_to_indicator_gen(k, len, idx, witness_vals.as_slice()));
     }
 }
