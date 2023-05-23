cd10fc031585ef37425712b868f5f542d2a6d10a
Xinding Wei <weixinding@gmail.com>
2023-05-23 11:45:21 -0700

    Add SafeType (#26)
    * Add SafeType
    
    * Refactor & add testing
    
    * Add doc comment
    
    * Refactor SafeChip
    
    * Move gen_proof/check_proof to utils
    
    * Fix merge issues

---
+++ halo2-base/src/safe_types/mod.rs
@@ -0,0 +1,146 @@
+pub use crate::{
+    gates::{
+        flex_gate::GateInstructions,
+        range::{RangeChip, RangeInstructions},
+    },
+    utils::ScalarField,
+    AssignedValue, Context,
+    QuantumCell::{self, Constant, Existing, Witness},
+};
+use std::cmp::{max, min};
+
+#[cfg(test)]
+pub mod tests;
+
+type RawAssignedValues<F> = Vec<AssignedValue<F>>;
+
+const BITS_PER_BYTE: usize = 8;
+
+/// SafeType's goal is to avoid out-of-range undefined behavior.
+/// When building circuits, it's common to use mulitple AssignedValue<F> to represent
+/// a logical varaible. For example, we might want to represent a hash with 32 AssignedValue<F>
+/// where each AssignedValue represents 1 byte. However, the range of AssignedValue<F> is much
+/// larger than 1 byte(0~255). If a circuit takes 32 AssignedValue<F> as inputs and some of them
+/// are actually greater than 255, there could be some undefined behaviors.
+/// SafeType gurantees the value range of its owned AssignedValue<F>. So circuits don't need to
+/// do any extra value checking if they take SafeType as inputs.
+/// TOTAL_BITS is the number of total bits of this type.
+/// BYTES_PER_ELE is the number of bytes of each element.
+#[derive(Clone, Debug)]
+pub struct SafeType<F: ScalarField, const BYTES_PER_ELE: usize, const TOTAL_BITS: usize> {
+    // value is stored in little-endian.
+    value: RawAssignedValues<F>,
+}
+
+impl<F: ScalarField, const BYTES_PER_ELE: usize, const TOTAL_BITS: usize>
+    SafeType<F, BYTES_PER_ELE, TOTAL_BITS>
+{
+    /// Number of bytes of each element.
+    pub const BYTES_PER_ELE: usize = BYTES_PER_ELE;
+    /// Total bits of this type.
+    pub const TOTAL_BITS: usize = TOTAL_BITS;
+    /// Number of bits of each element.
+    pub const BITS_PER_ELE: usize = min(TOTAL_BITS, BYTES_PER_ELE * BITS_PER_BYTE);
+    /// Number of elements of this type.
+    pub const VALUE_LENGTH: usize =
+        (TOTAL_BITS + BYTES_PER_ELE * BITS_PER_BYTE - 1) / (BYTES_PER_ELE * BITS_PER_BYTE);
+
+    // new is private so Safetype can only be constructed by this crate.
+    fn new(raw_values: RawAssignedValues<F>) -> Self {
+        assert!(raw_values.len() == Self::VALUE_LENGTH, "Invalid raw values length");
+        Self { value: raw_values }
+    }
+
+    /// Return values in littile-endian.
+    pub fn value(&self) -> &RawAssignedValues<F> {
+        &self.value
+    }
+}
+
+/// Represent TOTAL_BITS with the least number of AssignedValue<F>.
+/// (2^(F::NUM_BITS) - 1) might not be a valid value for F. e.g. max value of F is a prime in [2^(F::NUM_BITS-1), 2^(F::NUM_BITS) - 1]
+#[allow(type_alias_bounds)]
+type CompactSafeType<F: ScalarField, const TOTAL_BITS: usize> =
+    SafeType<F, { ((F::NUM_BITS - 1) / 8) as usize }, TOTAL_BITS>;
+
+/// SafeType for bool.
+pub type SafeBool<F> = CompactSafeType<F, 1>;
+/// SafeType for uint8.
+pub type SafeUint8<F> = CompactSafeType<F, 8>;
+/// SafeType for uint16.
+pub type SafeUint16<F> = CompactSafeType<F, 16>;
+/// SafeType for uint32.
+pub type SafeUint32<F> = CompactSafeType<F, 32>;
+/// SafeType for uint64.
+pub type SafeUint64<F> = CompactSafeType<F, 64>;
+/// SafeType for uint128.
+pub type SafeUint128<F> = CompactSafeType<F, 128>;
+/// SafeType for uint256.
+pub type SafeUint256<F> = CompactSafeType<F, 256>;
+/// SafeType for bytes32.
+pub type SafeBytes32<F> = SafeType<F, 1, 256>;
+
+/// Chip for SafeType
+pub struct SafeTypeChip<'a, F: ScalarField> {
+    range_chip: &'a RangeChip<F>,
+}
+
+impl<'a, F: ScalarField> SafeTypeChip<'a, F> {
+    /// Construct a SafeTypeChip.
+    pub fn new(range_chip: &'a RangeChip<F>) -> Self {
+        Self { range_chip }
+    }
+
+    /// Convert a vector of AssignedValue(treated as little-endian) to a SafeType.
+    /// The number of bytes of inputs must equal to the number of bytes of outputs.
+    /// This function also add contraints that a AssignedValue in inputs must be in the range of a byte.
+    pub fn raw_bytes_to<const BYTES_PER_ELE: usize, const TOTAL_BITS: usize>(
+        &self,
+        ctx: &mut Context<F>,
+        inputs: RawAssignedValues<F>,
+    ) -> SafeType<F, BYTES_PER_ELE, TOTAL_BITS> {
+        let element_bits = SafeType::<F, BYTES_PER_ELE, TOTAL_BITS>::BITS_PER_ELE;
+        let bits = TOTAL_BITS;
+        assert!(
+            inputs.len() * BITS_PER_BYTE == max(bits, BITS_PER_BYTE),
+            "number of bits doesn't match"
+        );
+        self.add_bytes_constraints(ctx, &inputs, bits);
+        // inputs is a bool or uint8.
+        if bits == 1 || element_bits == BITS_PER_BYTE {
+            return SafeType::<F, BYTES_PER_ELE, TOTAL_BITS>::new(inputs);
+        };
+
+        let byte_base = (0..BYTES_PER_ELE)
+            .map(|i| Witness(self.range_chip.gate.pow_of_two[i * BITS_PER_BYTE]))
+            .collect::<Vec<_>>();
+        let value = inputs
+            .chunks(BYTES_PER_ELE)
+            .map(|chunk| {
+                self.range_chip.gate.inner_product(
+                    ctx,
+                    chunk.to_vec(),
+                    byte_base[..chunk.len()].to_vec(),
+                )
+            })
+            .collect::<Vec<_>>();
+        SafeType::<F, BYTES_PER_ELE, TOTAL_BITS>::new(value)
+    }
+
+    fn add_bytes_constraints(
+        &self,
+        ctx: &mut Context<F>,
+        inputs: &RawAssignedValues<F>,
+        bits: usize,
+    ) {
+        let mut bits_left = bits;
+        for input in inputs {
+            let num_bit = min(bits_left, BITS_PER_BYTE);
+            self.range_chip.range_check(ctx, *input, num_bit);
+            bits_left -= num_bit;
+        }
+    }
+
+    // TODO: Add comprasion. e.g. is_less_than(SafeUint8, SafeUint8) -> SafeBool
+    // TODO: Add type castings. e.g. uint256 -> bytes32/uint32 -> uint64
+}
---
+++ halo2-base/src/safe_types/tests.rs
@@ -0,0 +1,242 @@
+use crate::halo2_proofs::{
+    halo2curves::bn256::{Bn256, Fr, G1Affine},
+    plonk::{create_proof, verify_proof, Circuit, ProvingKey, VerifyingKey},
+    poly::commitment::ParamsProver,
+    poly::kzg::{
+        commitment::KZGCommitmentScheme, commitment::ParamsKZG, multiopen::ProverSHPLONK,
+        multiopen::VerifierSHPLONK, strategy::SingleStrategy,
+    },
+    transcript::{
+        Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer, TranscriptWriterBuffer,
+    },
+};
+
+use crate::{
+    gates::{
+        builder::{RangeCircuitBuilder, GateThreadBuilder},
+        RangeChip,
+    },
+    halo2_proofs::{
+        plonk::keygen_pk,
+        plonk::{keygen_vk, Assigned},
+    },
+};
+use rand::rngs::OsRng;
+use itertools::Itertools;
+use super::*;
+use std::env;
+
+/// helper function to generate a proof with real prover
+pub fn gen_proof(
+    params: &ParamsKZG<Bn256>,
+    pk: &ProvingKey<G1Affine>,
+    circuit: impl Circuit<Fr>,
+) -> Vec<u8> {
+    let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+    create_proof::<
+        KZGCommitmentScheme<Bn256>,
+        ProverSHPLONK<'_, Bn256>,
+        Challenge255<_>,
+        _,
+        Blake2bWrite<Vec<u8>, G1Affine, _>,
+        _,
+    >(params, pk, &[circuit], &[&[]], OsRng, &mut transcript)
+    .expect("prover should not fail");
+    transcript.finalize()
+}
+
+/// helper function to verify a proof
+pub fn check_proof(
+    params: &ParamsKZG<Bn256>,
+    vk: &VerifyingKey<G1Affine>,
+    proof: &[u8],
+    expect_satisfied: bool,
+) {
+    let verifier_params = params.verifier_params();
+    let strategy = SingleStrategy::new(params);
+    let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(proof);
+    let res = verify_proof::<
+        KZGCommitmentScheme<Bn256>,
+        VerifierSHPLONK<'_, Bn256>,
+        Challenge255<G1Affine>,
+        Blake2bRead<&[u8], G1Affine, Challenge255<G1Affine>>,
+        SingleStrategy<'_, Bn256>,
+    >(verifier_params, vk, strategy, &[&[]], &mut transcript);
+
+    if expect_satisfied {
+        assert!(res.is_ok());
+    } else {
+        assert!(res.is_err());
+    }
+}
+
+// soundness checks for `raw_bytes_to` function
+fn test_raw_bytes_to_gen<const BYTES_PER_ELE: usize, const TOTAL_BITS: usize>(k: u32, raw_bytes: &[Fr], outputs: &[Fr], expect_satisfied: bool) {
+    // first create proving and verifying key
+    let mut builder = GateThreadBuilder::<Fr>::keygen();
+    let lookup_bits = 3;
+    env::set_var("LOOKUP_BITS", lookup_bits.to_string());
+    let range_chip = RangeChip::<Fr>::default(lookup_bits);
+    let safe_type_chip = SafeTypeChip::new(&range_chip);
+
+    let dummy_raw_bytes = builder.main(0).assign_witnesses((0..raw_bytes.len()).map(|_| Fr::zero()).collect::<Vec<_>>());
+
+    let safe_value = safe_type_chip.raw_bytes_to::<BYTES_PER_ELE, TOTAL_BITS>(
+        builder.main(0), 
+        dummy_raw_bytes);
+    // get the offsets of the safe value cells for later 'pranking'
+    let safe_value_offsets = safe_value.value().iter().map(|v| v.cell.unwrap().offset).collect::<Vec<_>>();
+    // set env vars
+    builder.config(k as usize, Some(9));
+    let circuit = RangeCircuitBuilder::keygen(builder);
+
+    let params = ParamsKZG::setup(k, OsRng);
+    // generate proving key
+    let vk = keygen_vk(&params, &circuit).unwrap();
+    let pk = keygen_pk(&params, vk, &circuit).unwrap();
+    let vk = pk.get_vk(); // pk consumed vk
+
+    // now create different proofs to test the soundness of the circuit
+    let gen_pf = |inputs: &[Fr], outputs: &[Fr]| {
+        let mut builder = GateThreadBuilder::<Fr>::prover();
+        let range_chip = RangeChip::<Fr>::default(lookup_bits);
+        let safe_type_chip = SafeTypeChip::new(&range_chip);
+    
+        let assigned_raw_bytes = builder.main(0).assign_witnesses(inputs.to_vec());
+        safe_type_chip.raw_bytes_to::<BYTES_PER_ELE, TOTAL_BITS>(
+            builder.main(0), 
+            assigned_raw_bytes);
+        // prank the safe value cells
+        for (offset, witness) in safe_value_offsets.iter().zip_eq(outputs) {
+            builder.main(0).advice[*offset] = Assigned::<Fr>::Trivial(*witness);
+        }
+        let circuit = RangeCircuitBuilder::prover(builder, vec![vec![]]); // no break points
+        gen_proof(&params, &pk, circuit)
+    };
+    let pf = gen_pf(raw_bytes, outputs);
+    check_proof(&params, vk, &pf, expect_satisfied);
+}
+
+#[test]
+fn test_raw_bytes_to_bool() {
+    let k = 8;
+    test_raw_bytes_to_gen::<1, 1>(k, &[Fr::from(0)], &[Fr::from(0)], true);
+    test_raw_bytes_to_gen::<1, 1>(k, &[Fr::from(1)], &[Fr::from(1)], true);
+    test_raw_bytes_to_gen::<1, 1>(k, &[Fr::from(1)], &[Fr::from(0)], false);
+    test_raw_bytes_to_gen::<1, 1>(k, &[Fr::from(0)], &[Fr::from(1)], false);
+    test_raw_bytes_to_gen::<1, 1>(k, &[Fr::from(3)], &[Fr::from(0)], false);
+    test_raw_bytes_to_gen::<1, 1>(k, &[Fr::from(3)], &[Fr::from(1)], false);
+}
+
+#[test]
+fn test_raw_bytes_to_uint256() {
+    const BYTES_PER_ELE: usize = SafeUint256::<Fr>::BYTES_PER_ELE;
+    const TOTAL_BITS: usize = SafeUint256::<Fr>::TOTAL_BITS;
+    let k = 11;
+    // [0x0; 32] -> [0x0, 0x0]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(k, &[Fr::from(0); 32], &[Fr::from(0), Fr::from(0)], true);
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(1)].as_slice(), [Fr::from(0); 31].as_slice()].concat(), 
+        &[Fr::from(1), Fr::from(0)], true);
+    // [0x1, 0x2] + [0x0; 30] -> [0x201, 0x0]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(1), Fr::from(2)].as_slice(), [Fr::from(0); 30].as_slice()].concat(), 
+        &[Fr::from(0x201), Fr::from(0)], true);
+    // [[0xff; 32] -> [2^248 - 1, 0xff]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[Fr::from(0xff); 32], 
+        &[Fr::from_raw([0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffff]), Fr::from(0xff)], true);
+
+    // invalid raw_bytes, last bytes > 0xff
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(0); 31].as_slice(), [Fr::from(0x1ff)].as_slice()].concat(), 
+        &[Fr::from(0), Fr::from(0xff)], false);
+    // 0xff != 0xff00000000000000000000000000000000000000000000000000000000000000
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(0xff)].as_slice(), [Fr::from(0); 31].as_slice()].concat(), 
+        &[Fr::from(0), Fr::from(0xff)], false);
+    // outputs overflow
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[Fr::from(0xff); 32], 
+        &[Fr::from_raw([0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffffff, 0xffffffffffffff]), Fr::from(0x1ff)], false);
+}
+
+#[test]
+fn test_raw_bytes_to_uint64() {
+    const BYTES_PER_ELE: usize = SafeUint64::<Fr>::BYTES_PER_ELE;
+    const TOTAL_BITS: usize = SafeUint64::<Fr>::TOTAL_BITS;
+    let k = 10;
+    // [0x0; 8] -> [0x0]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(k, &[Fr::from(0); 8], &[Fr::from(0)], true);
+    // [0x1, 0x2] + [0x0; 6] -> [0x201]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(1), Fr::from(2)].as_slice(), [Fr::from(0); 6].as_slice()].concat(), 
+        &[Fr::from(0x201)], true);
+    // [[0xff; 8] -> [2^64-1]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[Fr::from(0xff); 8], 
+        &[Fr::from(0xffffffffffffffff)], true);
+
+    // invalid raw_bytes, last bytes > 0xff
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(0); 7].as_slice(), [Fr::from(0x1ff)].as_slice()].concat(), 
+        &[Fr::from(0xff00000000000000)], false);
+    // 0xff != 0xff00000000000000000000000000000000000000000000000000000000000000
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(0xff)].as_slice(), [Fr::from(0); 7].as_slice()].concat(), 
+        &[Fr::from(0xff00000000000000)], false);
+    // outputs overflow
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[Fr::from(0xff); 8], 
+        &[Fr::from_raw([0xffffffffffffffff, 0x1, 0x0, 0x0])], false);
+}
+
+#[test]
+fn test_raw_bytes_to_bytes32() {
+    const BYTES_PER_ELE: usize = SafeBytes32::<Fr>::BYTES_PER_ELE;
+    const TOTAL_BITS: usize = SafeBytes32::<Fr>::TOTAL_BITS;
+    let k = 10;
+    // [0x0; 32] -> [0x0; 32]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(k, &[Fr::from(0); 32], &[Fr::from(0); 32], true);
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(1)].as_slice(), [Fr::from(0); 31].as_slice()].concat(), 
+        &[[Fr::from(1)].as_slice(), [Fr::from(0); 31].as_slice()].concat(), true);
+    // [0x1, 0x2] + [0x0; 30] -> [0x201, 0x0]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(1), Fr::from(2)].as_slice(), [Fr::from(0); 30].as_slice()].concat(), 
+        &[[Fr::from(1), Fr::from(2)].as_slice(), [Fr::from(0); 30].as_slice()].concat(), true);
+    // [[0xff; 32] -> [2^248 - 1, 0xff]
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[Fr::from(0xff); 32], 
+        &[Fr::from(0xff); 32], true);
+
+    // invalid raw_bytes, last bytes > 0xff
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k,
+        &[[Fr::from(0); 31].as_slice(), [Fr::from(0x1ff)].as_slice()].concat(), 
+        &[[Fr::from(0); 31].as_slice(), [Fr::from(0x1ff)].as_slice()].concat(), false);
+    // 0xff != 0xff00000000000000000000000000000000000000000000000000000000000000
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[[Fr::from(0xff)].as_slice(), [Fr::from(0); 31].as_slice()].concat(),
+        &[[Fr::from(0); 31].as_slice(), [Fr::from(0xff)].as_slice()].concat(), false);
+    // outputs overflow
+    test_raw_bytes_to_gen::<BYTES_PER_ELE, TOTAL_BITS>(
+        k, 
+        &[Fr::from(0xff); 32], 
+        &[Fr::from(0x1ff); 32], false);
+}
