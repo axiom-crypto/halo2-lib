402dac423da4259f97788827848ee0d9443454d2
mmagician <marcin.gorny.94@protonmail.com>
2023-08-07 19:25:08 -0600

    Add `sub_mul` to GateInstructions (#102)
    * Add `sub_mul` to GateInstructions
    
    * Add `sub_mul` prop test

--- halo2-base/src/gates/tests/flex_gate.rs
+++ halo2-base/src/gates/tests/flex_gate.rs
@@ -31,6 +31,15 @@ pub fn test_sub<F: ScalarField>(inputs: &[QuantumCell<F>]) -> F {
     *a.value()
 }
 
+#[test_case(&[1, 1, 1].map(Fr::from).map(Witness) => Fr::from(0) ; "sub_mul(): 1 - 1 * 1 == 0")]
+pub fn test_sub_mul<F: ScalarField>(inputs: &[QuantumCell<F>]) -> F {
+    let mut builder = GateThreadBuilder::mock();
+    let ctx = builder.main(0);
+    let chip = GateChip::default();
+    let a = chip.sub_mul(ctx, inputs[0], inputs[1], inputs[2]);
+    *a.value()
+}
+
 #[test_case(Witness(Fr::from(1)) => -Fr::from(1) ; "neg(): 1 -> -1")]
 pub fn test_neg<F: ScalarField>(a: QuantumCell<F>) -> F {
     let mut builder = GateThreadBuilder::mock();
