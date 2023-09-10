ce584cc14c470340dc650fd007f401aaa92302ff
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-09-10 14:17:45 -0700

    feat(keccak): add `ingestion` module for Rust native input formatting

--- hashes/zkevm/src/keccak/coprocessor/mod.rs
+++ hashes/zkevm/src/keccak/coprocessor/mod.rs
@@ -2,6 +2,8 @@
 pub mod circuit;
 /// Module of encoding raw inputs to coprocessor circuit lookup keys.
 pub mod encode;
+/// Module for Rust native processing of input bytes into resized fixed length format to match vanilla circuit LoadedKeccakF
+pub mod ingestion;
 /// Module of Keccak coprocessor circuit output.
 pub mod output;
 /// Module of Keccak coprocessor circuit constant parameters.
