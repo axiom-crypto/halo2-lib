cc87b08243964eb9ced93ea24dc6a2e150217087
Xinding Wei <weixinding@gmail.com>
2023-09-13 01:17:36 -0700

    Try t=7

--- hashes/zkevm/src/keccak/coprocessor/param.rs
+++ hashes/zkevm/src/keccak/coprocessor/param.rs
@@ -5,8 +5,8 @@ pub const OUTPUT_COL_IDX_KEY: usize = 0;
 pub const OUTPUT_COL_IDX_HASH_LO: usize = 1;
 pub const OUTPUT_COL_IDX_HASH_HI: usize = 2;
 
-pub const POSEIDON_T: usize = 3;
-pub const POSEIDON_RATE: usize = 2;
+pub const POSEIDON_T: usize = 7;
+pub const POSEIDON_RATE: usize = 6;
 pub const POSEIDON_R_F: usize = 8;
-pub const POSEIDON_R_P: usize = 57;
+pub const POSEIDON_R_P: usize = 56;
 pub const POSEIDON_SECURE_MDS: usize = 0;
