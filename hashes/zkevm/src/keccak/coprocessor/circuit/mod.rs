bf4a0847b6d7d53a67e2169d3d42bb40e68df80b
Xinding Wei <weixinding@gmail.com>
2023-09-28 10:28:03 -0700

    WIP

--- hashes/zkevm/src/keccak/coprocessor/circuit/mod.rs
+++ hashes/zkevm/src/keccak/coprocessor/circuit/mod.rs
@@ -1,3 +1,72 @@
 pub mod leaf;
 #[cfg(test)]
 mod tests;
+
+use std::{
+    collections::HashSet,
+    error,
+    fmt::Debug,
+    fs::{File, OpenOptions},
+    hash::Hash,
+    path::Path,
+};
+
+use hex::serde;
+use serde::{de::DeserializeOwned, Serialize};
+
+pub trait BenchRecord<P>: Serialize + DeserializeOwned {
+    fn get_parameter(&self) -> P;
+}
+
+pub fn bench_circuit<P: Eq + Debug + Hash, BR: BenchRecord<P>>(
+    name: &str,
+    parameter_set: Vec<P>,
+    filed_names: &[&'static str],
+    bench_impl: fn(P) -> BR,
+) -> Result<(), Box<dyn error::Error>> {
+    let rt = tokio::runtime::Builder::new_current_thread().enable_all().build()?;
+    let instance_type = rt.block_on(get_instance_type())?;
+    let filepath = format!("bench_{name}_{instance_type}.csv");
+    ensure_file_exists(&filepath, filed_names);
+    // Read the CSV file.
+    let mut rdr = csv::ReaderBuilder::new().has_headers(true).from_path(&filepath)?;
+
+    let mut saved_parameters = HashSet::<P>::new();
+
+    for result in rdr.deserialize() {
+        let rd: BR = result?;
+        saved_parameters.insert(rd.get_parameter());
+    }
+    // Open the CSV file in append mode.
+    let file = OpenOptions::new().append(true).open(&filepath)?;
+
+    let mut wtr = csv::WriterBuilder::new().has_headers(false).from_writer(file);
+
+    for parameter in parameter_set {
+        println!("Benchmarking {:?}", parameter);
+        let e = saved_parameters.get(&parameter);
+        if e.is_some() {
+            println!("Already have results. Continue");
+            continue;
+        }
+        let br = bench_impl(parameter);
+        wtr.serialize(br)?;
+        wtr.flush()?;
+    }
+    Ok(())
+}
+
+pub async fn get_instance_type() -> Result<String, Box<dyn error::Error>> {
+    let url = "http://169.254.169.254/latest/meta-data/instance-type";
+    let resp = reqwest::get(url).await?.text().await?;
+    Ok(resp)
+}
+
+fn ensure_file_exists<P: AsRef<Path>>(path: P, filed_names: &[&'static str]) {
+    let path_ref = path.as_ref();
+    if !path_ref.exists() {
+        let f = File::create(path_ref).unwrap();
+        let mut wtr = csv::Writer::from_writer(f);
+        wtr.write_record(filed_names).unwrap();
+    }
+}
--- hashes/zkevm/src/keccak/coprocessor/circuit/tests/aggregation.rs
+++ hashes/zkevm/src/keccak/coprocessor/circuit/tests/aggregation.rs
@@ -60,18 +60,19 @@ fn create_snark_leaf_circuit_commit(k: usize, unusable_rows: usize, capacity: us
 
 // RUST_LOG=info cargo t single_layer_aggregate_leaf_circuits_commit::_1_rows_per_round_single_shard -- --nocapture
 // Aggregation will be huge, will likely OOM
-#[test_case(15, 23, 1308, 1, 23;"1 rows_per_round, single shard")]
+// #[test_case(15, 23, 1308, 1, 23;"1 rows_per_round, single shard")]
 // RUST_LOG=info cargo t single_layer_aggregate_leaf_circuits_commit::_9_rows_per_round_single_shard -- --nocapture
-#[test_case(15, 55, 144, 1, 20;"9 rows_per_round, single shard")]
-#[test_case(15, 67, 61, 1, 18;"21 rows_per_round, single shard")]
+// #[test_case(15, 55, 144, 1, 20;"9 rows_per_round, single shard")]
+// #[test_case(15, 67, 61, 1, 18;"21 rows_per_round, single shard")]
 fn single_layer_aggregate_leaf_circuits_commit(
     leaf_k: usize,
     unusable_rows: usize,
-    capacity: usize,
+    target_capacity: usize,
     num_shards: usize,
     agg_k: u32,
 ) {
     let _ = env_logger::builder().is_test(true).try_init();
+    let capacity = target_capacity / num_shards;
 
     fs::remove_file("keccak_leaf.pk").unwrap_or_default();
     let shard = create_snark_leaf_circuit_commit(leaf_k, unusable_rows, capacity);
@@ -94,7 +95,7 @@ fn single_layer_aggregate_leaf_circuits_commit(
     };
 
     let mut circuit = aggregate_shards(CircuitBuilderStage::Keygen);
-    circuit.calculate_params(Some(9));
+    circuit.calculate_params(Some(unusable_rows));
     let pk = gen_pk(&params, &circuit, None);
     let agg_config = circuit.params();
     let break_points = circuit.break_points();
@@ -104,4 +105,44 @@ fn single_layer_aggregate_leaf_circuits_commit(
         .use_params(agg_config)
         .use_break_points(break_points);
     gen_snark_shplonk(&params, &pk, circuit, None::<&str>);
+}
+
+#[derive(Serialize, Deserialize, FieldNames, Default)]
+struct HeaderBenchRecord {
+    k: usize,
+    n_pow: usize,
+    capacity: usize,
+    proof_ms: u128,
+    verify_ms: u128,
+    gate_advice_phase0: usize,
+    gate_advice_phase1: usize,
+    lookup_advice_phase0: usize,
+    rlc_advice: usize,
+}
+
+impl BenchRecord<(usize, usize)> for HeaderBenchRecord {
+    fn get_parameter(&self) -> (usize, usize) {
+        (self.k, self.n_pow)
+    }
+}
+
+#[test]
+#[ignore = "bench"]
+fn bench_keccak_aggregation() {
+    let agg_capacity_list = [200, 500, 1000, 5000];
+    let num_shards_list = [1, 2, 4, 8];
+    let leaf_k = 20;
+    let agg_k = 20;
+    let unusable_rows = 55;
+    for agg_capacity in agg_capacity_list {
+        for num_shards in num_shards_list {
+            single_layer_aggregate_leaf_circuits_commit(
+                leaf_k,
+                unusable_rows,
+                agg_capacity,
+                num_shards,
+                agg_k,
+            );
+        }
+    }
 }
