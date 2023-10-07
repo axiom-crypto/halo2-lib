e432e88a98486f87d037a4dacacc297b31f6fbfd
Timofey Luin <timofey.luin@gmail.com>
2023-10-07 22:14:03 +0200

    complete test

--- halo2-ecc/src/bls12_381/tests/hash_to_curve.rs
+++ halo2-ecc/src/bls12_381/tests/hash_to_curve.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, marker::PhantomData};
+use std::{fs::{File, self}, marker::PhantomData, io::{BufReader, BufRead}};
 
 use super::*;
 use crate::{
@@ -7,10 +7,16 @@ use crate::{
     fields::{FieldChip, FpStrategy},
 };
 use halo2_base::{
-    gates::RangeChip, halo2_proofs::plonk::Error, utils::BigPrimeField, AssignedValue, Context,
-    QuantumCell,
+    gates::RangeChip,
+    halo2_proofs::{
+        halo2curves::{bls12_381::{hash_to_curve::HashToCurve, G2Affine, G2}, CurveAffine},
+        plonk::Error,
+    },
+    utils::BigPrimeField,
+    AssignedValue, Context, QuantumCell,
 };
 extern crate pairing;
+use crate::group::Curve;
 use itertools::Itertools;
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
@@ -51,7 +57,7 @@ impl<F: BigPrimeField> HashInstructions<F> for Sha256MockChip<F> {
             })
             .collect_vec();
 
-        let output_bytes = Sha256::digest(input_bytes)
+        let output_bytes = Sha256::digest(&input_bytes)
             .into_iter()
             .map(|b| ctx.load_witness(F::from(b as u64)))
             .collect_vec();
@@ -65,6 +71,8 @@ fn hash_to_g2_test<F: BigPrimeField>(
     params: HashToCurveCircuitParams,
     msg: Vec<u8>,
 ) {
+    use crate::halo2_base::halo2_proofs::halo2curves::bls12_381::hash_to_curve::ExpandMsgXmd as ExpandMsgXmdNative;
+    const DST: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
     let fp_chip = FpChip::<F>::new(range, params.limb_bits, params.num_limbs);
     let fp2_chip = Fp2Chip::new(&fp_chip);
 
@@ -72,22 +80,25 @@ fn hash_to_g2_test<F: BigPrimeField>(
 
     let h2c_chip = HashToCurveChip::new(&sha256, &fp2_chip);
 
-    let hp = h2c_chip
+    let assigned_msghash = h2c_chip
         .hash_to_curve::<ExpandMsgXmd>(
             ctx,
-            msg.into_iter().map(|b| QuantumCell::Witness(F::from(b as u64))),
-            b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_",
+            msg.iter().copied().map(|b| QuantumCell::Witness(F::from(b as u64))),
+            DST,
         )
         .unwrap();
 
-    println!(
-        "msghash: {:?}",
-        (fp2_chip.get_assigned_value(&hp.x.into()), fp2_chip.get_assigned_value(&hp.y.into()))
-    );
+    let msghash = G2Affine::from_xy(
+        fp2_chip.get_assigned_value(&assigned_msghash.x.into()),
+        fp2_chip.get_assigned_value(&assigned_msghash.y.into()),
+    ).unwrap();
 
     // Verify off-circuit
+    let msghash_control =
+        <G2 as HashToCurve<ExpandMsgXmdNative<sha2::Sha256>>>::hash_to_curve(&msg, DST).to_affine();
 
     // Compare the 2 results
+    assert_eq!(msghash, msghash_control);
 }
 
 #[test]
@@ -106,3 +117,51 @@ fn test_hash_to_g2() {
         hash_to_g2_test(ctx, range, params, test_input);
     })
 }
+
+
+#[test]
+fn bench_pairing() -> Result<(), Box<dyn std::error::Error>> {
+    let config_path = "configs/bls12_381/bench_hash_to_curve.config";
+    let bench_params_file =
+        File::open(config_path).unwrap_or_else(|e| panic!("{config_path} does not exist: {e:?}"));
+    fs::create_dir_all("results/bls12_381").unwrap();
+    fs::create_dir_all("data").unwrap();
+
+    let results_path = "results/bls12_381/pairing_bench.csv";
+    let mut fs_results = File::create(results_path).unwrap();
+    writeln!(fs_results, "degree,num_advice,num_lookup,num_fixed,lookup_bits,limb_bits,num_limbs,proof_time,proof_size,verify_time")?;
+
+    let bench_params_reader = BufReader::new(bench_params_file);
+    for line in bench_params_reader.lines() {
+        let bench_params: HashToCurveCircuitParams =
+            serde_json::from_str(line.unwrap().as_str()).unwrap();
+        let k = bench_params.degree;
+        println!("---------------------- degree = {k} ------------------------------",);
+
+        let test_input = vec![0u8; 32];
+        let stats = base_test().k(k).lookup_bits(bench_params.lookup_bits).bench_builder(
+            test_input.clone(),
+            test_input,
+            |pool, range, test_input| {
+                hash_to_g2_test(pool.main(), range, bench_params, test_input);
+            },
+        );
+
+        writeln!(
+            fs_results,
+            "{},{},{},{},{},{},{},{:?},{},{:?}",
+            bench_params.degree,
+            bench_params.num_advice,
+            bench_params.num_lookup_advice,
+            bench_params.num_fixed,
+            bench_params.lookup_bits,
+            bench_params.limb_bits,
+            bench_params.num_limbs,
+            stats.proof_time.time.elapsed(),
+            stats.proof_size,
+            stats.verify_time.time.elapsed()
+        )?;
+    }
+    Ok(())
+}
+
