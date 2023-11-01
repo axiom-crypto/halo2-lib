d45950368839c7766c7486a8cb9efcd4c1978dd7
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-11-01 14:33:56 -0700

    feat: change `memory` example to use `BasicDynLookupConfig` for testing

--- halo2-base/src/virtual_region/tests/lookups/memory.rs
+++ halo2-base/src/virtual_region/tests/lookups/memory.rs
@@ -1,11 +1,17 @@
-use crate::halo2_proofs::{
-    arithmetic::Field,
-    circuit::{Layouter, SimpleFloorPlanner, Value},
-    dev::MockProver,
-    halo2curves::bn256::Fr,
-    plonk::{keygen_pk, keygen_vk, Advice, Circuit, Column, ConstraintSystem, Error},
-    poly::Rotation,
+use std::any::TypeId;
+
+use crate::{
+    halo2_proofs::{
+        arithmetic::Field,
+        circuit::{Layouter, SimpleFloorPlanner},
+        dev::MockProver,
+        halo2curves::bn256::Fr,
+        plonk::{keygen_pk, keygen_vk, Assigned, Circuit, ConstraintSystem, Error},
+    },
+    virtual_region::lookups::basic::BasicDynLookupConfig,
+    AssignedValue, ContextCell,
 };
+use halo2_proofs_axiom::plonk::FirstPhase;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 use test_log::test;
 
@@ -16,7 +22,6 @@ use crate::{
     },
     utils::{
         fs::gen_srs,
-        halo2::raw_assign_advice,
         testing::{check_proof, gen_proof},
         ScalarField,
     },
@@ -26,15 +31,13 @@ use crate::{
 #[derive(Clone, Debug)]
 struct RAMConfig<F: ScalarField> {
     cpu: FlexGateConfig<F>,
-    copy: Vec<[Column<Advice>; 2]>,
-    // dynamic lookup table
-    memory: [Column<Advice>; 2],
+    memory: BasicDynLookupConfig<2>,
 }
 
 #[derive(Clone, Default)]
 struct RAMConfigParams {
     cpu: FlexGateConfigParams,
-    copy_columns: usize,
+    num_lu_sets: usize,
 }
 
 struct RAMCircuit<F: ScalarField, const CYCLES: usize> {
@@ -89,30 +92,12 @@ impl<F: ScalarField, const CYCLES: usize> Circuit<F> for RAMCircuit<F, CYCLES> {
     }
 
     fn configure_with_params(meta: &mut ConstraintSystem<F>, params: Self::Params) -> Self::Config {
-        let k = params.cpu.k;
-        let mut cpu = FlexGateConfig::configure(meta, params.cpu);
-        let copy: Vec<_> = (0..params.copy_columns)
-            .map(|_| {
-                [(); 2].map(|_| {
-                    let advice = meta.advice_column();
-                    meta.enable_equality(advice);
-                    advice
-                })
-            })
-            .collect();
-        let mem = [meta.advice_column(), meta.advice_column()];
+        let memory = BasicDynLookupConfig::new(meta, || FirstPhase, params.num_lu_sets);
+        let cpu = FlexGateConfig::configure(meta, params.cpu);
 
-        for copy in &copy {
-            meta.lookup_any("dynamic memory lookup table", |meta| {
-                let mem = mem.map(|c| meta.query_advice(c, Rotation::cur()));
-                let copy = copy.map(|c| meta.query_advice(c, Rotation::cur()));
-                vec![(copy[0].clone(), mem[0].clone()), (copy[1].clone(), mem[1].clone())]
-            });
-        }
         log::info!("Poisoned rows: {}", meta.minimum_rows());
-        cpu.max_rows = (1 << k) - meta.minimum_rows();
 
-        RAMConfig { cpu, copy, memory: mem }
+        RAMConfig { cpu, memory }
     }
 
     fn configure(_: &mut ConstraintSystem<F>) -> Self::Config {
@@ -124,21 +109,41 @@ impl<F: ScalarField, const CYCLES: usize> Circuit<F> for RAMCircuit<F, CYCLES> {
         config: Self::Config,
         mut layouter: impl Layouter<F>,
     ) -> Result<(), Error> {
+        // Make purely virtual cells so we can raw assign them
+        let memory = self.memory.iter().enumerate().map(|(i, value)| {
+            let idx = Assigned::Trivial(F::from(i as u64 + 1));
+            let idx = AssignedValue {
+                value: idx,
+                cell: Some(ContextCell::new(TypeId::of::<RAMConfig<F>>(), 0, i)),
+            };
+            let value = Assigned::Trivial(*value);
+            let value = AssignedValue {
+                value,
+                cell: Some(ContextCell::new(TypeId::of::<RAMConfig<F>>(), 1, i)),
+            };
+            [idx, value]
+        });
+        config.memory.assign_virtual_table_to_raw(
+            layouter.namespace(|| "memory"),
+            memory,
+            (!self.cpu.witness_gen_only()).then_some(&self.cpu.copy_manager),
+        );
+
         layouter.assign_region(
-            || "RAM Circuit",
+            || "cpu",
             |mut region| {
-                // Raw assign the private memory inputs
-                for (i, &value) in self.memory.iter().enumerate() {
-                    // I think there will always be (0, 0) in the table so we index starting from 1
-                    let idx = Value::known(F::from(i as u64 + 1));
-                    raw_assign_advice(&mut region, config.memory[0], i, idx);
-                    raw_assign_advice(&mut region, config.memory[1], i, Value::known(value));
-                }
                 self.cpu.assign_raw(
                     &(config.cpu.basic_gates[0].clone(), config.cpu.max_rows),
                     &mut region,
                 );
-                self.ram.assign_raw(&config.copy, &mut region);
+                Ok(())
+            },
+        )?;
+        config.memory.assign_managed_lookups(layouter.namespace(|| "memory accesses"), &self.ram);
+        // copy constraints at the very end for safety:
+        layouter.assign_region(
+            || "copy constraints",
+            |mut region| {
                 self.cpu.copy_manager.assign_raw(&config.cpu.constants, &mut region);
                 Ok(())
             },
@@ -155,7 +160,6 @@ fn test_ram_mock() {
     let memory: Vec<_> = (0..mem_len).map(|_| Fr::random(&mut rng)).collect();
     let ptrs = [(); CYCLES].map(|_| rng.gen_range(0..memory.len()));
     let usable_rows = 2usize.pow(k) - 11; // guess
-    let copy_columns = CYCLES / usable_rows + 1;
     let params = RAMConfigParams::default();
     let mut circuit = RAMCircuit::new(memory, ptrs, params, false);
     circuit.compute();
@@ -166,7 +170,7 @@ fn test_ram_mock() {
         num_advice_per_phase: vec![num_advice],
         num_fixed: 1,
     };
-    circuit.params.copy_columns = copy_columns;
+    circuit.params.num_lu_sets = CYCLES / usable_rows + 1;
     MockProver::run(k, &circuit, vec![]).unwrap().assert_satisfied();
 }
 
@@ -182,7 +186,6 @@ fn test_ram_prover() {
     let ptrs = [0; CYCLES];
 
     let usable_rows = 2usize.pow(k) - 11; // guess
-    let copy_columns = CYCLES / usable_rows + 1;
     let params = RAMConfigParams::default();
     let mut circuit = RAMCircuit::new(memory, ptrs, params, false);
     circuit.compute();
@@ -192,7 +195,7 @@ fn test_ram_prover() {
         num_advice_per_phase: vec![num_advice],
         num_fixed: 1,
     };
-    circuit.params.copy_columns = copy_columns;
+    circuit.params.num_lu_sets = CYCLES / usable_rows + 1;
 
     let params = gen_srs(k);
     let vk = keygen_vk(&params, &circuit).unwrap();
